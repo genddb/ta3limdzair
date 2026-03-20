9e5ba19ebe9d2123f28db81f9cb86d4553df53c5
genddb <geniusdz88@gmail.com>
2026-03-20 04:14:33 +0100

    Create bac-countdown.js

---
+++ bac-countdown.js
@@ -0,0 +1,3 @@
+function updateBacTimer(){const examDate=new Date("June 7, 2026 08:00:00").getTime();const now=new Date().getTime();const gap=examDate-now;if(gap<=0){document.getElementById("bac-timer").innerHTML="<h3>انطلقت الامتحانات.. بالتوفيق للجميع!</h3>";return}
+const second=1000;const minute=second*60;const hour=minute*60;const day=hour*24;const d=Math.floor(gap/day);const h=Math.floor((gap%day)/hour);const m=Math.floor((gap%hour)/minute);const s=Math.floor((gap%minute)/second);document.getElementById("days").innerText=d<10?"0"+d:d;document.getElementById("hours").innerText=h<10?"0"+h:h;document.getElementById("minutes").innerText=m<10?"0"+m:m;document.getElementById("seconds").innerText=s<10?"0"+s:s}
+setInterval(updateBacTimer,1000);updateBacTimer()
